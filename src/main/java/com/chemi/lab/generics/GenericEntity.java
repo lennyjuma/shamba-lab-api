fcd5b8300565c2cb2da057b3c12d1148fe6f29f6
lennyjuma <lennyndungu34@gmail.com>
2024-06-29 14:07:32 +0300

    added generic classes

---
+++ src/main/java/com/chemi/lab/generics/GenericEntity.java
@@ -0,0 +1,12 @@
+package com.chemi.lab.generics;
+
+public interface GenericEntity <T>{
+
+    // update current instance with provided data
+    void update(T source);
+
+    String getId();
+
+    // based on current data create new instance with new id
+    T createNewInstance();
+}
---
+++ src/main/java/com/chemi/lab/generics/GenericService.java
@@ -0,0 +1,45 @@
+package com.chemi.lab.generics;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public abstract class GenericService<T extends GenericEntity <T>> {
+
+    private final GenericRepository<T> repository;
+
+    public GenericService(GenericRepository<T> repository) {
+        this.repository = repository;
+    }
+
+    public Page<T> getPage(Pageable pageable){
+        return repository.findAll(pageable);
+    }
+
+    public T get(String id){
+        return repository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Id not found")
+        );
+    }
+
+    @Transactional
+    public T update(T updated){
+        T dbDomain = get(updated.getId());
+        dbDomain.update(updated);
+
+        return repository.save(dbDomain);
+    }
+
+    @Transactional
+    public T create(T newDomain){
+        T dbDomain = newDomain.createNewInstance();
+        return repository.save(dbDomain);
+    }
+
+    @Transactional
+    public void delete(String id){
+        //check if object with this id exists
+        get(id);
+        repository.deleteById(id);
+    }
+}
