161fcfccf2cd68f69a980ff800108ec64190ebba
lenny ndungu <lennyndungu34@gmail.com>
2024-12-15 11:55:23 +0300

    Update KafkaProducerConfig.java
    updated kafka bootstrap server url

--- src/main/java/com/chemi/lab/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/chemi/lab/kafka/config/KafkaProducerConfig.java
@@ -20,7 +20,7 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-    @Value("localhost:29092")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServer;
 
     public Map<String,Object> producerConfig(){
